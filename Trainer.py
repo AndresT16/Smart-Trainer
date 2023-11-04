d08ddb5ceaac1e8411ff3c7716e78a4a0a1a282f
Jean Gaibor <149634460+jeacgaib@users.noreply.github.com>
2023-11-03 19:01:57 -0500

    Update Trainer.py
    Se aplico: 
    Recolorear imagen a RGB 
    Realizar detecciones 
    Recolorear de nuevo a BGR 
    Deteccion de renderizado

--- Trainer.py
+++ Trainer.py
@@ -7,12 +7,23 @@ deteccion_pose = mp.solutions.pose
 
 # transmision de video
 captura = cv2.VideoCapture(0)
-while captura.isOpened():
-    retorno , frame = captura.read()
-    cv2.imshow("Mediapipe Feed",frame)
-
-    if cv2.waitKey(10) & 0xFF == ord("q"):
-        break
-
+#Configurar mediapipe instancia 
+with deteccion_pose.Pose(min_deteccion_segura=0.5,min_seguimiento_segura=0.5) as pose:
+    while captura.isOpened():
+        retorno, frame = captura.read()
+        #Recolorear imagen a RGB
+        imagen = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        imagen.flags.writeable = False
+        #Realizar detecciones
+        resultado = pose.process(imagen)
+        #Recolorear de nuevo a BGR
+        imagen.flags.writeable = True
+        #Deteccion de renderizado 
+        mp.drawing.draw_landmarks(imagen,resultado.pose_landmarks, deteccion_pose.POSE_CONNECTIONS,
+                                 dibujo.DrawingSpec(color=(245,117,66), thickness= 2, circle_radius=2),
+                                 dibujo.DrawingSpec(color=(245,117,66), thickness= 2, circle_radius=2))
+        cv2.imshow("Mediapipe Feed",frame)
+        if cv2.waitKey(10) & 0xFF == ord("q"):
+            break
 captura.release()
 cv2.destroyAllWindows()    
