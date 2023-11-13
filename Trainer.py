cc6886b0b44f5c01717a35490ea3d550842e209a
Jean Gaibor <149634460+jeacgaib@users.noreply.github.com>
2023-11-12 21:41:24 -0500

    Update Trainer.py
    Correccion de erorres

--- Trainer.py
+++ Trainer.py
@@ -2,13 +2,13 @@ import cv2
 import mediapipe as mp
 import numpy as np
 
-dibujo = mp.solutions.drawing_utils
-deteccion_pose = mp.solutions.pose
+mp_dibujo = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
 
 # transmision de video
 captura = cv2.VideoCapture(0)
 #Configurar mediapipe instancia 
-with deteccion_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as pose:
+with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as pose:
     while captura.isOpened():
         retorno, frame = captura.read()
         #Recolorear imagen a RGB
@@ -19,9 +19,9 @@ with deteccion_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.
         #Recolorear de nuevo a BGR
         imagen.flags.writeable = True
         #Deteccion de renderizado 
-        mp.drawing.draw_landmarks(imagen,resultado.pose_landmarks, deteccion_pose.POSE_CONNECTIONS,
-                                 dibujo.DrawingSpec(color=(245,117,66), thickness= 2, circle_radius=2),
-                                 dibujo.DrawingSpec(color=(245,117,66), thickness= 2, circle_radius=2))
+        mp_dibujo.draw_landmarks(imagen,resultado.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                 mp_dibujo.DrawingSpec(color=(245,117,66), thickness= 2, circle_radius=2),
+                                 mp_dibujo.DrawingSpec(color=(245,117,66), thickness= 2, circle_radius=2))
         cv2.imshow("Mediapipe Feed",frame)
         if cv2.waitKey(10) & 0xFF == ord("s"):
             break
