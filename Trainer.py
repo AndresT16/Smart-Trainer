32076afe8177346df834f20434a823e9376e3c5a
Jean Gaibor <149634460+jeacgaib@users.noreply.github.com>
2023-11-12 22:59:49 -0500

    Update Trainer.py
    Se relizo: Extraccion puntos de referencia.- 
    1.Obtener las coordenadas 
    2.Calculo del ángulo
    3.Visualizar el ángulo

--- Trainer.py
+++ Trainer.py
@@ -14,7 +14,6 @@ def calcular_angulo(a,b,c):
 
     if angulo > 180.0:
         angulo = 360 - angulo
-
     return angulo
 
 # transmision de video
@@ -31,6 +30,28 @@ with mp_pose.Pose(min_detection_confidence=0.5,min_tracking_confidence=0.5) as p
         #Recolorear de nuevo a BGR
         imagen.flags.writeable = True
         imagen = cv2.cvtColor(imagen, cv2.COLOR_RGB2BGR)
+
+        #Extraccion puntos de referencia
+        try:
+            landmarks = resultado.pose_landmarks.landmark
+
+            # Obtener coordenadas
+            hombro = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                        landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            codo = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            muñeca = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+            # Calcular ángulo
+            angle = calcular_angulo(hombro, codo, muñeca)
+
+            # Visualizar ángulo
+            cv2.putText(imagen, str(angle),
+                        tuple(np.multiply(codo, [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                        )
+        except:
+            pass
         #Deteccion de renderizado 
         mp_dibujo.draw_landmarks(imagen,resultado.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                  mp_dibujo.DrawingSpec(color=(245,117,66), thickness= 2, circle_radius=2),
